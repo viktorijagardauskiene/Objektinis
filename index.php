f9a76afc12334b15416662a16399d9b3813668d3
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-07-14 19:19:54 +0300

    __construct funkcija

--- index.php
+++ index.php
@@ -5,14 +5,29 @@
 class Car {
 	public $make; // nurodom kintamuosius, reiksmiu suteikti nereikia, public - kad butu pasiekiami is visur
 	public $model;
+	public $owner;
 
+	public function __construct($owner) { // funkcija veikianti sukuriant objekta, pradzioje yra du apatiniai bruksniai, visada ivykdomas kai pagaminam nauja objekta
+		$this->owner = $owner;
+	}
+
+	public function drive($speed) { // nurodom objekto funkcija, kitaip vadinama metodu
+		echo "We are going somewhere at speed of $speed <br />";
+}
+		public function stop() { // nurodom objekto funkcija
+		echo "Breaking <br />";
+	}
 }
 
-$auto = new Car(); // sukuriamas naujas objektas
+$auto = new Car("Viktorija"); // sukuriamas naujas objektas, skliausteliuose priskirtas owner reiksme
 $auto->make = "Toyota"; // -> priskiriama objektui reiksme  
 $auto->model = "Avensis";
+$auto->drive(80); // priskiriama objektui funkcija (metodas), skliausteliuose nurodyta $speed reiksme
+$auto->stop();
 
-$auto2 = new Car(); // sukuriamas naujas objektas
+print_r($auto);
+
+/* $auto2 = new Car(); // sukuriamas naujas objektas
 $auto2->make = "BMW"; // -> priskiriama objektui reiksme  
 $auto2->model = "X";
 
@@ -31,6 +46,6 @@ $cars = [$auto, $auto2, $auto3]; // sukuriame objektu masyva
 print_r($cars);
 
 echo $auto3->make; // parodo auto3 make reiksme
-echo $cars[2]->make // parodo ta pacia auto3 make reiksme tik is masyvo cars
-
+echo $cars[2]->make; // parodo ta pacia auto3 make reiksme tik is masyvo cars
+*/
 
