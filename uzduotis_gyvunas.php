e68e2e8c6039264ab89ba16adc2bb5a4dc1af068
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-07-14 20:26:12 +0300

    nurodyti kokie bus kintamieji ir kokie bus metodai

---
+++ uzduotis_gyvunas.php
@@ -0,0 +1,28 @@
+<?php
+
+class Animal {
+	public $name;
+	public $countOfLegs;
+	public $hasFurr;
+	public $food;
+
+
+}
+
+public function run() {
+
+}
+
+public function talk() {
+	
+}
+
+public function eat() {
+	
+}
+
+public function sleep() {
+	
+}
+
+echo '$name is'.run();
